b8c802d461e8b2d46852012bb927cc064e1c1b5c
Joaquin <joaquinzache@hotmail.com>
2021-02-27 14:06:58 +0100

    Started working on a solution

--- server.c
+++ server.c
@@ -1,12 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 /* You will to add includes here */
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
 
 
+#define DEBUG
+int main(int argc, char *argv[])
+{
 
-int main(int argc, char *argv[]){
-  
   /* Do more magic */
-  
+  if (argc != 2)
+  {
+    printf("Usage: %s <ip>:<port> \n", argv[0]);
+    exit(1);
+  }
+  /*
+    Read first input, assumes <ip>:<port> syntax, convert into one string (Desthost) and one integer (port). 
+     Atm, works only on dotted notation, i.e. IPv4 and DNS. IPv6 does not work if its using ':'. 
+  */
+  char delim[] = ":";
+  char *Desthost = strtok(argv[1], delim);
+  char *Destport = strtok(NULL, delim);
+  if (Desthost == NULL || Destport == NULL)
+  {
+    printf("Usage: %s <ip>:<port> \n", argv[0]);
+    exit(1);
+  }
+  // *Desthost now points to a sting holding whatever came before the delimiter, ':'.
+  // *Dstport points to whatever string came after the delimiter.
 
+  /* Do magic */
+  int port = atoi(Destport);
+#ifdef DEBUG
+  printf("Host %s, and port %d.\n", Desthost, port);
+#endif
+
+  struct addrinfo hint, *servinfo, *p;
+  int rv;
+  int serverSock;
+
+  memset(&hint, 0, sizeof hint);
+  hint.ai_family = AF_UNSPEC;
+  hint.ai_socktype = SOCK_STREAM;
+
+  if ((rv = getaddrinfo(Desthost, Destport, &hint, &servinfo)) != 0)
+  {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+  for (p = servinfo; p != NULL; p = p->ai_next)
+  {
+    if ((serverSock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
+    {
+      printf("Socket creation failed.\n");
+      continue;
+    }
+    printf("Socket Created.\n");
+    rv = bind(serverSock, p->ai_addr, p->ai_addrlen);
+    if (rv == -1)
+    {
+      perror("Bind failed!\n");
+      close(serverSock);
+      continue;
+    }
+    break;
+  }
+
+  freeaddrinfo(servinfo);
+
+  if (p == NULL)
+  {
+    fprintf(stderr, "Client failed to create an apporpriate socket.\n");
+    exit(1);
+  }
 }
